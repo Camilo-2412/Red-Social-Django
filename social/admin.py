da6e8663e8ff48e313ebc8e67122ae117a7dcb10
Camilo Rodriguez <camilo.rod2412@gmail.com>
2021-07-24 10:15:07 -0500

    Proyector realizado para practicar django

---
+++ social/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post, profile, Relationship
+
+# Register your models here.
+admin.site.register(profile)
+admin.site.register(Post)
+admin.site.register(Relationship)
+
